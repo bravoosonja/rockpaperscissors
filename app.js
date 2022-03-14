b84ca1107ff2bb753eadc9f6a39870a0894996fa
bravoosonja <bravoosonja@gmail.com>
2022-03-14 15:23:43 +1100

    complete playRound

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
-//randomely returns either Rock, Paper, or Scissors
+//randomly returns either Rock, Paper, or Scissors
 function computerPlay() {
-  const items = ["Rock", "Paper", "Scissors"];
-  return items[~~(Math.random() * items.length)];
+  const items = ["rock", "paper", "scissors"];
+  return items[~~(Math.random() * items.length)].toString();
 }
 
+//plays a single round
+function playRound(playerSelection, computerSelection) {
+  computerSelection = computerSelection.toLowerCase();
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "Tie";
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      return "Computer won";
+    } else {
+      return "Player won";
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      return "Computer won";
+    } else {
+      return "Player won";
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "scissors") {
+      return "Computer won";
+    } else {
+      return "Player won";
+    }
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(`Computer play: ${computerSelection}`);
+console.log(`Player selection: ${playerSelection}`);
+console.log(playRound(playerSelection, computerSelection));
